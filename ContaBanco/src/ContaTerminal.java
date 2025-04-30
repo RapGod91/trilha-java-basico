77590fc29b7c8e0ef967af36ef16630d37f99983
RapGod91 <raphaelrodriguessilva@gmail.com>
2025-04-30 11:08:36 -0300

    ContaTerminal

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal{
+    public static void main(String[] args) throws Exception {
+        Scanner lerInfo = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência!");
+        int agencia = lerInfo.nextInt();
+
+        System.out.println("Por favor, digite o número da Conta!");
+        String numeroConta = lerInfo.next();
+
+        lerInfo.nextLine(); // Limpa o buffer do scanner
+
+        System.out.println("Por favor, digite o seu nome!");
+        String nomeCliente = lerInfo.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta!");
+        double saldo = lerInfo.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+        
+    }
+
+}
